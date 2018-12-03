66936c3beab09733760676eeb3811d2746229a78
Al-un <alun.sng@gmail.com>
2018-12-03 17:45:54 +0100

    removing useless welcome page

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -2,11 +2,7 @@
 
 # controller for static pages
 class StaticsController < ApplicationController
-  # welcome page
-  def welcome
-  end
 
   # misc info page
-  def misc_info
-  end
+  def misc_info; end
 end
