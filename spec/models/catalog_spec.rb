6c9a64b48d3413c0de517a434e8703bf657b5b7d
Al-un <alun.sng@gmail.com>
2018-12-03 16:24:28 +0100

    misc updates

--- spec/models/catalog_spec.rb
+++ spec/models/catalog_spec.rb
@@ -7,20 +7,20 @@ RSpec.describe Catalog, type: :model do
     # TMP-002 bad scope testing
     describe '.for_code' do
       it 'has proper query' do
-        c1 = create(:catalog, code: 'aabbcc')
+        create(:catalog, code: 'aabbcc')
         c2 = create(:catalog, code: 'ababab')
         c3 = create(:catalog, code: 'bababa')
-        c4 = create(:catalog, code: 'cdcdcd')
+        create(:catalog, code: 'cdcdcd')
         expect(Catalog.for_code('ba').all).to eq([c2, c3])
       end
     end
     # TMP-002 bad scope testing
     describe '.for_name' do
       it 'has proper query' do
-        c1 = create(:catalog, name: 'aabbcc')
+        create(:catalog, name: 'aabbcc')
         c2 = create(:catalog, name: 'ababab')
         c3 = create(:catalog, name: 'bababa')
-        c4 = create(:catalog, name: 'cdcdcd')
+        create(:catalog, name: 'cdcdcd')
         expect(Catalog.for_name('ba').all).to eq([c2, c3])
       end
     end
