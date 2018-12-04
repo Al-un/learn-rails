2657855f5baf9d7661977f0375ab6c7a36adcc0a
Al-un <alun.sng@gmail.com>
2018-12-04 15:36:02 +0100

    Rufo formatting

--- lib/log/ougai.rb
+++ lib/log/ougai.rb
@@ -6,24 +6,26 @@ module Log
     # Colors configuration
     COLORS = ::Ougai::Formatters::Colors::Configuration.new(
       severity: {
-        trace:  ::Ougai::Formatters::Colors::WHITE,
-        debug:  ::Ougai::Formatters::Colors::GREEN,
-        info:   ::Ougai::Formatters::Colors::CYAN,
-        warn:   ::Ougai::Formatters::Colors::YELLOW,
-        error:  ::Ougai::Formatters::Colors::RED,
-        fatal:  ::Ougai::Formatters::Colors::PURPLE
+        trace: ::Ougai::Formatters::Colors::WHITE,
+        debug: ::Ougai::Formatters::Colors::GREEN,
+        info: ::Ougai::Formatters::Colors::CYAN,
+        warn: ::Ougai::Formatters::Colors::YELLOW,
+        error: ::Ougai::Formatters::Colors::RED,
+        fatal: ::Ougai::Formatters::Colors::PURPLE
       },
       msg: :severity,
       datetime: {
-        default:  ::Ougai::Formatters::Colors::PURPLE,
-        error:    ::Ougai::Formatters::Colors::RED,
-        fatal:    ::Ougai::Formatters::Colors::RED
+        default: ::Ougai::Formatters::Colors::PURPLE,
+        error: ::Ougai::Formatters::Colors::RED,
+        fatal: ::Ougai::Formatters::Colors::RED
       }
     )
     # Data formatter: excluded fields
     EXCLUDED_FIELD = []
     # Lograge specific field exclusion
     LOGRAGE_REJECT = [:sql_queries, :sql_queries_count]
+    # Main log message format
+    MSG_FORMAT = '%<duration>6.2fms %<name>25s %<sql>s (%<type_casted_binds>s)'
 
     CONSOLE_FORMATTER = ::Ougai::Formatters::Customizable.new(
       format_msg: proc do |severity, datetime, _progname, data|
@@ -36,7 +38,7 @@ module Log
                                   .map { |key, val| "#{key}: #{val}" }
                                   .join(', ')
           msg = COLORS.color(:msg, lograge, severity)
-        # Standard text
+          # Standard text
         else
           msg = COLORS.color(:msg, msg, severity)
         end
@@ -53,14 +55,12 @@ module Log
           lograge_data = data[:request]
           if lograge_data.key?(:sql_queries)
             lograge_data[:sql_queries].map do |sql_query|
-              format('%<duration>6.2fms %<name>25s %<sql>s (%<type_casted_binds>s)', sql_query)
+              format(MSG_FORMAT, sql_query)
             end
-            .join("\n")
-          else
-            nil
+              .join("\n")
           end
-          
-        # Default styling
+
+          # Default styling
         else
           EXCLUDED_FIELD.each { |field| data.delete(field) }
           next nil if data.empty?
