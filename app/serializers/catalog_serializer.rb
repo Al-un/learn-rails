d2c088b3eca5023fb4a0f8450a6dab466910db5c
Al-un <alun.sng@gmail.com>
2018-12-19 23:07:06 +0100

    adding picture to serialization

--- app/serializers/catalog_serializer.rb
+++ app/serializers/catalog_serializer.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 # Serialize a single article
 class CatalogSerializer < ActiveModel::Serializer
+  include ActionView::Helpers::UrlHelper
+
   # mandatory attributes
   attributes :id, :code, :name
   # optional attributes
   attribute :description, unless: -> { object.description.nil? }
+  # optional attributes
+  attribute :picture_url, if: -> { object.picture.attached? }
   # associations
   belongs_to :user
   has_many :article_publications
@@ -13,5 +17,9 @@ class CatalogSerializer < ActiveModel::Serializer
 
   def publications_count
     object.article_publications.count
+  end
+
+  def picture_url
+    Rails.application.routes.url_helpers.rails_blob_path(object.picture, only_path: true)
   end
 end
